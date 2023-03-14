8288ff1079ee96585af3d3051a1f6e048720ed37
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-14 19:36:01 +0100

    feat: setup custom routes and mutations

--- plugin.js
+++ plugin.js
@@ -2,4 +2,47 @@
 'use strict'
 
 /** @param {import('fastify').FastifyInstance} app */
-module.exports = async function (app) {}
+module.exports = async function (app) {
+  app.graphql.extendSchema(`
+    extend type Mutation {
+      savePollWithOptions(poll: PollInput!, pollOptions: [PollOptionInput!]!): Poll
+      vote(pollId: ID!, optionId: ID!): Int
+    }
+  `)
+
+  async function savePollWithOptions(poll, pollOptions) {
+    const newPoll = await app.platformatic.entities.poll.save({ input: poll })
+
+    if (newPoll) {
+        pollOptions.map(async pollOption => {
+        const pollOptionToSave = { pollId: newPoll.id, ...pollOption }
+        return await app.platformatic.entities.pollOption.save({ input: pollOptionToSave })
+      })
+    }
+    return newPoll
+  }
+
+  async function vote(pollId, optionId) {
+    const { db, sql } = app.platformatic
+    const result = await db.query(sql`
+      UPDATE poll_options SET vote_counts = vote_counts + 1 WHERE id=${optionId} AND poll_id=${pollId} RETURNING vote_counts
+    `)
+    return result[0]?.vote_counts
+  }
+
+  app.graphql.defineResolvers({
+    Mutation: {
+      savePollWithOptions: async (_, { poll, pollOptions }) => await savePollWithOptions(poll, pollOptions),
+      vote: async (_, {pollId, optionId}) => vote(pollId, optionId)
+    }
+  })
+
+
+  app.post('/polls/new', async function (request, response) {
+    return { poll: await savePollWithOptions(request.body.poll, request.body.pollOptions) }
+  })
+
+  app.patch('/polls/:id/vote', async function (request, response) {
+    return { voteCounts: await vote(request.params.id, request.body.optionId)}
+  })
+}
